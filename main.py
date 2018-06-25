8fc9f71eedff96d3f9ccee6314988e41f886844f
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2018-06-25 22:58:35 +0530

    feat: update stop-words-removed-cases

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from stop_words import get_stop_words
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
 import codecs
 
 stop_words = list(get_stop_words('en'))  # About 900 stopwords
@@ -8,23 +8,30 @@ nltk_words = list(stopwords.words('english'))  # About 150 stopwords
 stop_words.extend(nltk_words)
 
 output = [w for w in stop_words if not w in nltk_words]
-unwanted_words = ['united', 'states', 'supreme', 'court', 'footnote', 'of']
+unwanted_words = ['united', 'states', 'supreme', 'court', 'footnote', 'of', 'et', 'al',
+                  'thus', 'since', 'hence', 'shall', 'upon']
+stop_words.extend(unwanted_words)
 
 for i in range(1, 2501):
     read_file = 'RawCases/' + str(i) + '.txt'
     write_file = 'StopWordsRemovedCases/' + str(i) + '.txt'
     case = codecs.open(read_file, 'r', 'iso-8859-1').read()
+    case = case.replace("[", "")
+    case = case.replace("]", "")
+
+    tagged_case = pos_tag(case.split())
+    word_tokens = [word for word, pos in tagged_case if pos != 'NNP']
 
     # tokenize the whole file into words and get their lowercase form
-    word_tokens = [w.lower() for w in word_tokenize(case)]
+    # word_tokens = [w.lower() for w in word_tokenize(case)]
 
     # remove stop words
-    new_tokens = [w for w in word_tokens if w not in stop_words]
+    new_tokens = [w for w in word_tokens if w.lower() not in stop_words]
 
     new_case = ""
 
     for token in new_tokens:
-        if token.isalpha() and len(token) > 1 and token not in unwanted_words:
+        if token.isalpha() and len(token) > 1:
             new_case += token + "\n"
 
     case_write = open(write_file, "w")
